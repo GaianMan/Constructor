1b12a1565e0b62d30b6d20575234d4fd97e6f855
Gayaneh Mandani <gaianbk@gmail.com>
2024-09-30 21:10:25 +0400

    couldnt get values by argument, couldnt assign by function itself, changed to this, works

--- Person.js
+++ Person.js
@@ -5,20 +5,28 @@
 // {other person name} is {older than / younger than / the same age as} me.
 
 function Person(name,age){
+    this.name=name;
+    this.age=age;
     //console.log(Person.arguments[1]);
 }
-Person.prototype.compareAge=function(arg){
-    const defAge=Person.arguments[1];
-
-    if(defAge<arg) return `${Person.arguments[0]} is older than me`;
-    //else if(defAge>arg){}
+Person.prototype.compareAge=function(newP){
+    // console.log(newP)
+    // console.log(newP.age)
+    // console.log(newP.name)
+    if(this.age<newP.age){
+        return `${newP.name} is older than me`;
+    }
+    else if(this.age>newP.age){
+        return `${newP.name} is Younger than me`;
+    }
+    else return `${newP.name} is same age as me`
 };
 
-const p1 //= Person("Samuel", 24);
-const p2 = Person("Joel", 36);
-const p3 = Person("Lily", 24);
+const p1 = new Person("Samuel", 24);
+const p2 = new Person("Joel", 36);
+const p3 =  new Person("Lily", 24);
 
-p1.compareAge(24);
-// p2.compareAge(p1);
-// p1.compareAge(p3);
 
+console.log(p2.compareAge(p1));
+console.log(p1.compareAge(p3));
+console.log(p1.compareAge(p2));
